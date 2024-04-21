dd2c208e63698b8ebd7fec164bed103158575d02
juanacostai <jpacosta@miuandes.cl>
2024-04-20 21:05:53 -0400

    mezclar codigo pickup con anterior

--- Assets/Scripts/Model/InventorySO.cs
+++ Assets/Scripts/Model/InventorySO.cs
@@ -12,7 +12,7 @@ namespace Inventory.Model {
         private List<InventoryItem> inventoryItems;
 
         [SerializeField]
-        public int Size { get; private set; } = 10;
+        public int Size { get; private set; } = 3;
 
         public event Action<Dictionary<int, InventoryItem>> OnInventoryUpdated; 
 
