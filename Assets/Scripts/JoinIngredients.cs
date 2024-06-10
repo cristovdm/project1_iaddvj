93162582c210847a877a392d1fa33d36676a8663
Ignacio Inostroza <iinostroza1@miuandes.cl>
2024-06-10 03:28:59 -0400

    Join ingredients final

--- Assets/Scripts/JoinIngredients.cs
+++ Assets/Scripts/JoinIngredients.cs
@@ -2,6 +2,7 @@ using Inventory.Model;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class JoinIngredients : MonoBehaviour
 {
@@ -32,11 +33,8 @@ public class JoinIngredients : MonoBehaviour
             InventoryItem item_1 = plateinventoryData.GetItemAt(0);
             InventoryItem item_2 = plateinventoryData.GetItemAt(1);
 
-            if ((item_1.item.name == "Bread" && item_2.item.name == "Carrot") ||
-                (item_1.item.name == "Carrot" && item_2.item.name == "Bread"))
-            {
-                isJoinable = true;
-            }
+            string combinedItemName = GetCombinedItemName(item_1.item.name, item_2.item.name);
+            isJoinable = !string.IsNullOrEmpty(combinedItemName);
         }
 
         joinButton.interactable = isJoinable;
@@ -52,20 +50,55 @@ public class JoinIngredients : MonoBehaviour
 
     void joinIngredients()
     {
+        InventoryItem item_1 = plateinventoryData.GetItemAt(0);
+        InventoryItem item_2 = plateinventoryData.GetItemAt(1);
 
-        plateinventoryData.RemoveItem(0, 1);
-        plateinventoryData.RemoveItem(1, 1);
+        string combinedItemName = GetCombinedItemName(item_1.item.name, item_2.item.name);
 
-        ItemSO joinedItem = ResourceManager.LoadResource<EdibleItemSO>("Tomato");
-        InventoryItem item = new InventoryItem
+        if (!string.IsNullOrEmpty(combinedItemName))
         {
-            item = joinedItem,
-            quantity = 1,
-            itemState = new System.Collections.Generic.List<ItemParameter>()
-        };
+            plateinventoryData.RemoveItem(0, 1);
+            plateinventoryData.RemoveItem(1, 1);
 
-        plateinventoryData.AddItem(item);
+            ItemSO joinedItem = ResourceManager.LoadResource<EdibleItemSO>(combinedItemName);
+            InventoryItem newItem = new InventoryItem
+            {
+                item = joinedItem,
+                quantity = 1,
+                itemState = new List<ItemParameter>()
+            };
+
+            plateinventoryData.AddItem(newItem);
+        }
 
         joinButton.interactable = false;
+    }
+
+    string GetCombinedItemName(string item1, string item2)
+    {
+        var combinations = new Dictionary<(string, string), string>
+        {
+            { ("FriedFish", "FriedEgg"), "FriedFishAndEgg" },
+            { ("FriedEgg", "FriedFish"), "FriedFishAndEgg" },
+            { ("CutTomato", "CutCorn"), "Salad" },
+            { ("CutCorn", "CutTomato"), "Salad" },
+            { ("FriedFish", "CutTomato"), "PescadoHorno" },
+            { ("CutTomato", "FriedFish"), "PescadoHorno" },
+            { ("FriedFish", "PanCortado"), "SandwichDePescado" },
+            { ("PanCortado", "FriedFish"), "SandwichDePescado" },
+            { ("TomatoSoup", "PanCortadoFrito"), "SopaTomateCrotones" },
+            { ("PanCortadoFrito", "TomatoSoup"), "SopaTomateCrotones" },
+            { ("PescadoCaldero", "CornSoup"), "CazuelaMarina" },
+            { ("CornSoup", "PescadoCaldero"), "CazuelaMarina" },
+            { ("SopaZanahoria", "Egg"), "CarrotCake" },
+            { ("Egg", "SopaZanahoria"), "CarrotCake" }
+        };
+
+        if (combinations.TryGetValue((item1, item2), out string result) ||
+            combinations.TryGetValue((item2, item1), out result))
+        {
+            return result;
+        }
+        return null;
     }
 }
