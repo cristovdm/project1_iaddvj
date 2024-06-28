d34fcc3dd4c62baad5e42542288d506a8d7ce4d1
Ignacio Inostroza <iinostroza1@miuandes.cl>
2024-06-27 23:30:00 -0400

    levels and rat fix

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -33,6 +33,16 @@ public class LevelManager : MonoBehaviour
         levelDishes.Add("1-2", new List<string> { "Tomato Soup", "Corn Soup", "HuevoDuro", "PescadoCaldero", "SopaZanahoria"});
 
         levelDishes.Add("1-3", new List<string> { "FriedFish", "FriedFishAndEgg", "FriedEgg", "SopaTomateCrotones", "FriedCarrotBreadSticks" });
+
+        levelDishes.Add("1-4", new List<string> { "PopCorn", "CarrotCake", "PescadoHorno", "tortillaZanahoria" });
+
+        levelDishes.Add("1-5", new List<string> { "SopaTomateCrotones", "SandwichDePescado", "CazuelaMarina" });
+
+        levelDishes.Add("1-6", new List<string> { "SopaTomateCrotones", "PopCorn", "CazuelaMarina", "tortillaZanahoria", "FriedCarrotBreadSticks","CarrotTomatoSalad" });
+
+        levelDishes.Add("1-7", new List<string> { "Tomatican", "FriedFishAndEgg" });
+
+        levelDishes.Add("1-8", new List<string> { "Tomatican", "CarrotCake", "FriedCarrotBreadSticks", "CazuelaMarina", "SopaTomateCrotones" });
     }
 
     public List<string> GetDishesForCurrentLevel()
@@ -44,7 +54,7 @@ public class LevelManager : MonoBehaviour
         }
         else
         {
-            return new List<string>(); // Nivel no definido
+            return new List<string>();
         }
     }
 
